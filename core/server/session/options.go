613f9db58a1d798bb25dd7f9f793f064d5b70d99
Antonio Pagano <apagano@wawand.co>
2024-07-29 11:14:00 -0500

    feature: adding options for the session middleware

--- core/server/session/options.go
+++ core/server/session/options.go
@@ -31,3 +31,31 @@ func WithSameSite(sameSite http.SameSite) Option {
 		store.Options.SameSite = sameSite
 	}
 }
+
+func WithPath(path string) Option {
+	return func(store *sessions.CookieStore) {
+		store.Options.Path = path
+	}
+}
+
+// WithMaxAge sets the maximum age for the session cookie.
+// A negative value means that the cookie is deleted when the browser is closed.
+func WithMaxAge(maxAge int) Option {
+	return func(store *sessions.CookieStore) {
+		store.Options.MaxAge = maxAge
+	}
+}
+
+// WithHTTPOnly sets the HttpOnly flag on the session cookie.
+func WithHTTPOnly(httpOnly bool) Option {
+	return func(store *sessions.CookieStore) {
+		store.Options.HttpOnly = httpOnly
+	}
+}
+
+// WithSecure sets the Secure flag on the session cookie.
+func WithSecureFlag(secure bool) Option {
+	return func(store *sessions.CookieStore) {
+		store.Options.Secure = secure
+	}
+}
